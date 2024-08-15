e8ac9eb08ba1fe71a52661eadb16c4f7b16805d5
Max Cascone <max@bitovi.com>
2024-08-15 15:23:57 -0500

    wip

--- actions/example-config-updater/src/index.ts
+++ actions/example-config-updater/src/index.ts
@@ -4,25 +4,29 @@ import { DefaultArtifactClient } from "@actions/artifact";
 
 enum Mode {
   CREATE = "create",
-  READ = "read",
+  READ   = "read",
+  WRITE  = "write"
 };
 
 export async function run() {
   try {
-    const mode = core.getInput("mode");
+    const mode           = core.getInput("mode");
+
     const artifactClient = new DefaultArtifactClient();
+    const configFileName = "config.json";
+    const artifactName   = "config";
+    const rootDir        = './';
+    let artID: number = 0;
     
     // create a config.json file
     if (mode == Mode.CREATE) {
       console.log("Creating config.json file");
 
-      const configFileName = "config.json";
-      const artifactName = "config";
-      const rootDir = './';
-      
       const content = {
         max: "cascone",
       };
+
+      console.log('content: ', content);
       
       // write the file
       fs.writeFileSync(configFileName, JSON.stringify(content, null, 2));
@@ -30,11 +34,43 @@ export async function run() {
       // upload file 'config.json' as an artifact named 'config'
       const files = [configFileName];
       const { id, size } = await artifactClient.uploadArtifact(artifactName, files, rootDir);
+      artID = id ?? 0;
 
-      console.log(`Created artifact with id: ${id} (bytes: ${size})`);
+      console.log(`Created artifact with id: ${artID} (bytes: ${size})`);
     }
+    // READ the artifact into a file and create an object
     else if (mode == Mode.READ) {
       console.log("reading config.json file");
+
+      // download the artifact
+      // parse the file into an object
+      const downloadResponse = await artifactClient.downloadArtifact(artID);
+
+      console.log('downloadResponse: ', downloadResponse);
+      const file = downloadResponse.downloadPath;
+      console.log('file: ', file);
+
+      let data: string;
+      
+      // read the file
+      if (file) {
+        data = fs.readFileSync(file, 'utf8'); // Assign a value to the variable inside the if block
+      } else {
+        throw new Error("File path is undefined.");
+      }
+      
+      // parse the file into an object
+      const config = JSON.parse(data);
+
+      console.log('config: ', config);
+
+
+    }
+    else if (mode == Mode.WRITE) {
+      console.log("writing config.json file");
+    }
+    else {
+      throw new Error("Invalid mode: " + mode);
     }
 
   } catch (error) {
